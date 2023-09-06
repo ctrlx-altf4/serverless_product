9600b5e16f940672347a235a5049346734daa3f9
Prajwal Pradhan <prajwalpradhan@Prajwals-MacBook-Pro.local>
2023-09-06 17:46:59 +0545

    feat: make api url an environment variable

--- src/lib/axios/index.ts
+++ src/lib/axios/index.ts
@@ -1,7 +1,8 @@
 import axios from "axios";
 
+const apiUrl = process.env.NEXT_PUBLIC_API_URL;
 const Axios = axios.create({
-  baseURL: "https://leojjvv0zb.execute-api.ap-northeast-2.amazonaws.com/",
+  baseURL: apiUrl,
   timeout: 1000,
 });
 
